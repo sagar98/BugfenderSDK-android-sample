1bf23528316db705b466b8a38cb77ff6286767f4
sagar98 <sagarmahajan19@gmail.com>
2018-10-08 21:23:35 +0530

    Update MainActivity.java

--- app/src/main/java/com/bugfender/sample/MainActivity.java
+++ app/src/main/java/com/bugfender/sample/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     
-    Log.i("Test","Webhook");
+    Log.i("Test","Webhooktest");
 
     //Bugfender logs
     //Bugfender debug log
